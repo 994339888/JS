bd63891d175503b033c383f5b1e567dc332c7128
123 <tianwei2026@gmail.com>
2025-11-20 18:32:39 +0800

    更新 tongzhi.js

--- tongzhi.js
+++ tongzhi.js
@@ -1,6 +1,19 @@
-// qgou.js — 测试脚本
+/*
+        ➪：JP APP — 通知测试脚本
+
+[rewrite_local]
+^https?:\/\/api\.mercari\.jp\/services\/home\/v2\/homefeed-contents url script-response-body https://raw.githubusercontent.com/994339888/JS/main/tongzhi.js
+
+[MITM]
+hostname = api.mercari.jp
+*/
+
 let obj = JSON.parse($response.body);
 
-$notify("Mercari 脚本已生效", "接口被触发", "返回数量：" + Object.keys(obj).length);
+$notify(
+    "Mercari 脚本已生效",
+    "接口被触发",
+    "返回数量：" + Object.keys(obj).length
+);
 
 $done({ body: JSON.stringify(obj) });
